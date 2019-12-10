4d18d63606d2822f7c70cfbcc3b49ba7459e4b0a
Albertus Agung <albertus.ag@outlook.com>
2019-12-10 18:35:44 +0900

    add temp code

---
+++ Assets/MusicRunner/MidiController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Devices;
+
+public class MidiController : MonoBehaviour
+{
+    public MidiFile BgmMidi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string outputDeviceName = "";
+        foreach (var outputDevice in OutputDevice.GetAll())
+        {
+            Debug.Log(outputDevice.Name);
+            outputDeviceName = outputDevice.Name;
+        }
+
+        string appDataPath = Application.dataPath;
+        string midiPath = appDataPath + "/MusicRunner/Audio/Track01.mid";
+        using (OutputDevice outputDevice = OutputDevice.GetByName(outputDeviceName))
+        {
+
+           MidiFile.Read(midiPath).Play(outputDevice);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
