e092674c546c27effd37ea395f8fe570e68f0ffd
Albertus Agung <albertus.ag@outlook.com>
2019-12-16 03:33:17 +0900

    update

--- Assets/MusicRunner/MidiController.cs
+++ Assets/MusicRunner/MidiController.cs
@@ -10,6 +10,8 @@ public class MidiController : MonoBehaviour
 {
     public GameObject player;
     public AudioSource audioSource;
+    public AudioClip jumpClip;
+    public AudioClip crouchClip;
     private OutputDevice outputDevice;
     private Playback playback;
     private MidiFile midiFile;
@@ -33,15 +35,15 @@ public class MidiController : MonoBehaviour
     {
         string midiPath = Application.dataPath + "/MusicRunner/Audio/Track01.mid";
         Debug.Log(midiPath);
-        midiFile = MidiFile.Read(midiPath);
+        //midiFile = MidiFile.Read(midiPath);
 
-        outputDevice = OutputDevice.GetByName("Microsoft GS Wavetable Synth");
-        playback = midiFile.GetPlayback(outputDevice, new MidiClockSettings
-        {
-            CreateTickGeneratorCallback = interval => null
-        });
+        //outputDevice = OutputDevice.GetByName("Microsoft GS Wavetable Synth");
+        //playback = midiFile.GetPlayback(outputDevice, new MidiClockSettings
+        //{
+          //  CreateTickGeneratorCallback = interval => null
+        //});
 
-        StartCoroutine(PlayMusic());
+        //StartCoroutine(PlayMusic());
     }
 
     protected IEnumerator PlayMusic() 
@@ -76,24 +78,70 @@ public class MidiController : MonoBehaviour
 
     protected float GetColliderRadius(Bounds bound)
     {
-        float r = bound.size.x > bound.size.y ? bound.size.x : bound.size.y;
+        float r = bound.size.x > bound.size.y ? bound.size.x*2 : bound.size.y*2;
 
         return r;
     }
 
+    private float x_water;
+    private float curr_water = 0;
+    private float x_spikes;
+    private float curr_spikes = 0;
+    private float x_fly;
+    private float curr_fly = 0;
     void OnTriggerEnter2D(Collider2D item)
     {
+        x_water = this.gameObject.transform.position.x;
+        
+
         if (item.gameObject.tag == "kill")
         {
-            
+            if (item.gameObject.name.Equals("spikes"))
+            {
+                x_spikes = this.gameObject.transform.position.x;
+                if ((curr_spikes == 0) || (curr_spikes + 3 < x_spikes))
+                {
+                    curr_spikes = x_spikes;
+                    audioSource.clip = jumpClip;
+                    audioSource.Play();
+                    Debug.Log("spikes");
+
+                }
+            }
+            if (item.gameObject.name.Equals("evilBlock") || item.gameObject.name.Equals("evilSaw"))
+            {
+                x_fly = this.gameObject.transform.position.x;
+                if ((curr_fly == 0) || (curr_fly + 3 < x_fly))
+                {
+                    curr_fly = x_fly;
+                    audioSource.clip = crouchClip;
+                    audioSource.Play();
+                    Debug.Log("fly");
+
+                }
+            }
         }
         else if (item.gameObject.tag == "water")
         {
+            x_water = this.gameObject.transform.position.x;
+            if ((curr_water == 0) || (curr_water+5 < x_water))
+            {
+                curr_water = x_water;
+                audioSource.clip = jumpClip;
+                audioSource.Play();
+                Debug.Log("water");
+                
+            }
             
         }
         //else if (item.gameObject.tag == "coin" || (item.gameObject.tag == "coinL" && leftArm) || (item.gameObject.tag == "coinR" && rightArm) || (item.gameObject.tag == "coinB" && isBendDown))
         //{
             
         //}
+    }
+
+    void OnTriggerExit2D(Collider2D item)
+    {
+        
     }
 }
